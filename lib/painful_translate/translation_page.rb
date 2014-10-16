68deb630b51f4998a417cb78c7c34d14c4250f97
Thomas Chen <foxnewsnetwork@gmail.com>
2014-10-15 22:41:39 -0700

    buffed wait cooldown from 3 seconds to 2 seconds

--- lib/painful_translate/translation_page.rb
+++ lib/painful_translate/translation_page.rb
@@ -4,7 +4,7 @@ class PainfulTranslate::TranslationPage
     @session = session
   end
   def translation_output_strings
-    sleep 3.seconds # give some time for Jewgle to look the page
+    sleep 2.seconds # give some time for Jewgle to look the page
     session.find(:xpath, '//*[@id="result_box"]').all(:css, 'span.hps').map(&:text)
   end
 end
